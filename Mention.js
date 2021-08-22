f437414f94da7fde6730cf7808221d0f241ad1b4
agostinhodev <agostinho.developer@gmail.com>
2021-08-22 17:55:03 -0300

    feat: :sparkles: FInalizada adição de funcionalidades

--- Mention.js
+++ Mention.js
@@ -1,27 +1,39 @@
 $( document ).ready(function() {
-   
-    $('#input').autocomplete({
 
-        serviceUrl: './users.json',
-        type: 'GET',
-        width: 'auto',
-        autoSelectFirst: true,
-        delimiter: "@",
-        params: {
+    $("#input").keyup(function (event){
 
-        },
-        minChars: 1,
-        noCache: true,
-        appendTo: $('#result'),  
-       
-        onSelect: function (suggestion) {
-            
-           console.log(suggestion);
+        if(event.keyCode === 50){
+
+            $('#input').autocomplete({
+
+                serviceUrl: './users.json',
+                type: 'GET',
+                width: 'auto',
+                autoSelectFirst: true,
+                delimiter: "@",
+                params: {
+        
+                },
+                minChars: 1,
+                noCache: true,
+                appendTo: $('#result'),  
+               
+                onSelect: function (suggestion) {
+                    
+                   console.log(suggestion);
+        
+                }        
+              
+            });
+
+        } else if(event.keyCode === 32 || event.keyCode === 8){
+
+            $('#input').autocomplete( "disable" );
+            $('#result').html('');
 
         }
 
-      
-    });
+    })    
 
 
 });
